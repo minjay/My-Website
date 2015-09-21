8baa0f4fae1c8abe2cf42788b580ed383d541e04
minjay <mjfan@ucar.edu>
2015-09-20 19:58:10 -0700

    first commit

---
+++ branding.js
@@ -0,0 +1,7 @@
+function ucbheader(){
+	document.write('<a target="_blank" href="http://anson.ucdavis.edu/index.html"><img src="ucb.png" alt="University of Colorado at Boulder" /></a>');	
+}
+
+function ucbfooter(){
+	document.write('<a target="_blank" href="http://www.ucdavis.edu"><img src="wordmark.png" alt="University of Colorado at Boulder" /></a>');	
+}
